445b06835d451ffcc0e53f9e3f6bea019f17ab77
GreerPage <greerishere@gmail.com>
2021-08-04 18:15:54 -0400

    properly formatted json

--- ptfj/initial.py
+++ ptfj/initial.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 from pathlib import Path
+from .deal_with_json import writejson
 
 class Initial():
     """perform actions necessary for setup"""
@@ -11,8 +12,11 @@ class Initial():
         self.home = str(Path.home())
         self.p = self.home + 'Documents/.ptfj' if sys.platform == 'win32' else self.home + '/.ptfj'
         self.has_run = os.path.isdir(self.p)
+        self.json = self.p + '/tags.json'
 
     def initial(self):
         """Creates ptfj directory"""
 
         os.mkdir(self.p)
+        writejson(self.json, {"tags":{}})
+        
