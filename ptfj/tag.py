baaf30c8c73e69e3e9157043926b2d7e16ca876e
GreerPage <greerishere@gmail.com>
2021-08-04 18:15:22 -0400

    added tag function

--- ptfj/tag.py
+++ ptfj/tag.py
@@ -8,11 +8,20 @@ class Tagging():
     """Handle tagging"""
 
     def __init__(self, file):
-        self.file = file
+        self.path = file
+        self.file = readjson(self.path)
+        self.tags = self.file['tags']
     
     def create_tag(self, name):
         """Create a tag"""
 
-        tags = readjson(self.file)['tags']
-        
-        tags[name] = []
+        self.tags[name] = []
+
+    def tag(self, t, f):
+        """Tag a photo"""
+
+        if not t in self.tags:
+            self.create_tag(t)
+
+        self.tags[t].append(f)
+        writejson(self.path, self.file)
