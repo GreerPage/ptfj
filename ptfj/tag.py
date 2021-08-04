d2590174eba439c55f8c1f8fc0fe99ca5016ade3
GreerPage <greerishere@gmail.com>
2021-08-04 17:55:29 -0400

    added function to create tag

--- ptfj/tag.py
+++ ptfj/tag.py
@@ -2,10 +2,17 @@
 import os 
 import json
 
-from deal_with_json import readjson, writejson
+from .deal_with_json import readjson, writejson
 
 class Tagging():
     """Handle tagging"""
 
     def __init__(self, file):
         self.file = file
+    
+    def create_tag(self, name):
+        """Create a tag"""
+
+        tags = readjson(self.file)['tags']
+        
+        tags[name] = []
