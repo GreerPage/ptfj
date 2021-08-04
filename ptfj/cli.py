ecba4745f5b87b0d69bfc5b0538ccc030f8b9ea9
GreerPage <greerishere@gmail.com>
2021-08-04 18:54:37 -0400

    added cli and error functions

--- ptfj/cli.py
+++ ptfj/cli.py
@@ -2,7 +2,29 @@
 import os
 import sys
 
+from .tag import Tagging
+from .initial import Initial
+
+
+def error():
+	print('Usage: ptfj [ FILE_PATH ] [ TAG ]')
+	print('Try \'ptfj --help\' for more information.')
+
 def cli():
     """handles command line input"""
+
+    args = sys.argv
+    args.pop(0)
+
+    if args == []:
+        error()
+    
+    elif args == ['--help']:
+        print('Usage: ptfj [ FILE_PATH ] [ TAG ]')
+        print('Example: ptfj jackson.jpeg sillygeese')
+        print()
+        print('Listing: ptfj list [ TAG ]')
+        print('Example: ptfj list sillygeese')  
+
 
     
